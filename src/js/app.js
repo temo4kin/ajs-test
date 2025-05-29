a7582c64cf2c1e629c2cf57c274660fa20b450e1
Temo4kin <temo4kinn@gmail.com>
2025-05-29 22:58:07 +0300

    Add function to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,14 @@ import sum from './basic';
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+
+function getHealthOfHeroes(obj) {
+  if (obj.health >= 50) {
+    return healthy;
+  } else if (obj.health < 50 && obj.health > 15 ) {
+    return wounded;
+  } else if (obj.health <= 15) {
+    return critical;
+  }
+}
