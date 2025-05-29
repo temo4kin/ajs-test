1e9a2654c2916f9e03d78b4d2ae2ba6920e72cf3
Temo4kin <temo4kinn@gmail.com>
2025-05-29 21:47:30 +0300

    Update files

--- src/js/http.js
+++ src/js/http.js
@@ -1,7 +1,7 @@
 export function httpGet(url) {
-    throw new Error(url);
-  }
-  
-  export function httpPost(url) {
-    throw new Error(url);
-  }
+  throw new Error(url);
+}
+
+export function httpPost(url) {
+  throw new Error(url);
+}
